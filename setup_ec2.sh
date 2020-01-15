016e5a2c3b8b562630bb968ebe8b53b6b60fb70e
josh McGrath <josh.mcgrath08@gmail.com>
2020-01-15 12:41:20 -0500

    Configure EC2 instance to do security upgrades automatically

--- setup_ec2.sh
+++ setup_ec2.sh
@@ -9,11 +9,17 @@ ELASTIC_IP="$2"
 # and cf_template.json
 CLIENT_NAME='client'
 
-apt-get update && apt-get install awscli --assume-yes
-
+# Configure VPN and generate client key
 "${HERE}/setup_vpn.sh" apply
 "${HERE}/add_client.sh" "$CLIENT_NAME" "$ELASTIC_IP"
 
-S3_KEY="${CLIENT_NAME}.ovpn"
+# Install AWS CLI and upload client key to S3
+apt-get update && apt-get install awscli --assume-yes
+aws s3 cp "/etc/openvpn/${CLIENT_NAME}.ovpn" "s3://${S3_BUCKET_NAME}/${CLIENT_NAME}.ovpn"
 
-aws s3 cp "/etc/openvpn/${CLIENT_NAME}.ovpn" "s3://${S3_BUCKET_NAME}/${S3_KEY}"
+# Configure unattended upgrades for Ubuntu
+apt-get install unattended-upgrades update-notifier-common --assume-yes
+dpkg-reconfigure --frontend noninteractive --priority=low unattended-upgrades
+AUC=/etc/apt/apt.conf.d/20auto-upgrades
+echo "APT::Periodic::Verbose \"1\";" >> "$AUC"
+echo "Unattended-Upgrade::Automatic-Reboot \"true\";" >> "$AUC"
